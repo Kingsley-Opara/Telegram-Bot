2b98dd83e38816f31ce9d619abfe222c11f9162e
Kingsley-Opara <oparaudochukwu277@gmail.com>
2024-11-06 01:47:10 +0100

    updated the requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from config import get_config
+from config import get_config 
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
 
 config = get_config()
 
 BOT_USERNAME = config.BOT_USERNAME
 
-print(BOT_USERNAME)
+
 
